9e926c8a872068a464c7930587b1110a06b4c1b1
Rachaely <46507257+Rachaely@users.noreply.github.com>
2019-01-29 02:08:01 -0700

    Update convertsigs.c

--- convertsigs.c
+++ convertsigs.c
@@ -8,54 +8,53 @@
 
 #define MAX_TEXT 20
 
+
 int ascii, element_pos = 0, start = 0;
+char res[MAX_TEXT];
 //ascii is the ascii number of the letter sent though signals, 8 bit
 //element_pos points the 8 bit of binary ascii from left to right
 void handler(int signal)
 {
-    
-    
+
     if (signal == SIGUSR1)
     {
-        
-        
-        ascii += 1<<(7-element_pos);
-        
-        
-        
+
+        ascii += 1 << (7 - element_pos);
     }
     else if (signal == SIGUSR2)
     {
-        
-        
-        
     }
-    if (++element_pos == 8){
-            element_pos = 0;
-        } 
-    
+    if (++element_pos == 8)
+    {
+        element_pos = 0;
+    }
+
     //printf("pointer is %d, ascii is %d\n",element_pos, ascii);
     if (element_pos == 0)
-    {   
-        if (start == 0){
-            printf("!");
-            start++;
-        }
+    {
+        res[start] = ascii;
+        start++;
+
         
-        printf("%c", ascii);
-        if (ascii == 0){
+        if (ascii == 10)
+        {
+            printf("!%s\n",res);
             start = 0;
+            for(int i = 0;i<strlen(res) +1;i++){
+                res[i] = 0;
+            }
+            ascii = 0;
+            element_pos = 0;
         }
         ascii = 0;
         element_pos = 0;
         
     }
-
-
 }
 
 int main(int argc, char const *argv[])
 {
+
     printf("Own PID: %d\n", getpid());
     // print pid of this process
 
@@ -68,6 +67,13 @@ int main(int argc, char const *argv[])
 
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
+        #if defined(SINGLE)
+        int single_mode = 1;
+    #else
+        int single_mode = 0;
+    #endif
+
+    int sent = 0;
 
     pid_t pid;
     scanf("%d", &pid);
@@ -75,46 +81,47 @@ int main(int argc, char const *argv[])
 
     while (1)
     {
-        
-        
-        char sent_message[MAX_TEXT];
-        
-        scanf("%s", sent_message);
-        
-        //check if user enter a word
-        if (sent_message[0] >= 33 && sent_message[0] <= 126)
-        {   
-            strcat(sent_message,"\n");
 
-            
-            for (int i = 0; i < strlen(sent_message)+1; i++)
-            
+        char sent_message[MAX_TEXT];
+
+        fgets(sent_message,MAX_TEXT,stdin);
+
+        //check if user enter a word
+        if (sent_message[0] >= 33)
+        {
+            //strcat(sent_message, "\n");
+
+            for (int i = 0; i < strlen(sent_message); i++)
+
             //for each letter in the message
             {
-                
+
                 for (int j = 7; j >= 0; --j)
                 //encode letter into binary, order of bit is from left to right
                 {
                     if (sent_message[i] & (1 << j))
                     {
-                        printf("1");
+                        //printf("1");
                         kill(pid, SIGUSR1);
                     }
                     else
                     {
-                       printf("0");
+                        //printf("0");
                         kill(pid, SIGUSR2);
                     }
+                    
                     usleep(300);
 
                     //putchar((sent_message[i]&(1<<j))?'1':'0');
                 }
+                
             }
+            
+            
         }
-
-        
-        
-        
+        for(int i = 0; i <strlen(sent_message);i++){
+                sent_message[i] = 0;
+            }
     }
 
     return 0;
