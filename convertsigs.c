1993b16ddea2c8e1608f9166f0ce8596fbf5a4ca
Rachaely <46507257+Rachaely@users.noreply.github.com>
2019-01-29 23:02:25 -0700

    Update convertsigs.c

--- convertsigs.c
+++ convertsigs.c
@@ -5,49 +5,116 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_TEXT 20
 
-
-int ascii, element_pos = 0, start = 0;
+int ascii, element_pos = 0, start = 0, sender_mode = 0;
 char res[MAX_TEXT];
+struct timespec start_time = {0, 0}, end_time = {0, 0};
+unsigned long elapsed_time;
+
+//return start_time - end_time/ 10e-6
+long elapsed_time_ms(const struct timespec *start_time, const struct timespec *end_time)
+{
+    unsigned long sec, nsec;
+    if (start_time == NULL || end_time == NULL)
+    {
+        return 0;
+    }
+    sec = end_time->tv_sec - start_time->tv_sec;
+    nsec = end_time->tv_nsec - start_time->tv_nsec;
+    if (sec < 0)
+    {
+        return 0;
+    }
+    if (nsec < 0)
+    {
+        sec--;
+        nsec += 1000000000;
+    }
+    return sec * 1000000 + nsec / 1000;
+}
+
 //ascii is the ascii number of the letter sent though signals, 8 bit
 //element_pos points the 8 bit of binary ascii from left to right
 void handler(int signal)
 {
 
-    if (signal == SIGUSR1)
+    // get the mode of the sender
+    switch (sender_mode)
     {
-
-        ascii += 1 << (7 - element_pos);
-    }
-    else if (signal == SIGUSR2)
-    {
-    }
-    if (++element_pos == 8)
-    {
-        element_pos = 0;
-    }
-
-    //printf("pointer is %d, ascii is %d\n",element_pos, ascii);
-    if (element_pos == 0)
-    {
-        res[start] = ascii;
-        start++;
-
-        
-        if (ascii == 10)
+    case 0:
+        switch (signal)
         {
-            printf("!%s\n",res);
-            start = 0;
-            for(int i = 0;i<strlen(res) +1;i++){
-                res[i] = 0;
+        case SIGUSR1:
+            sender_mode = 1;
+            break;
+        case SIGUSR2:
+            sender_mode = 2;
+            break;
+        default:
+            break;
+        }
+        break;
+
+    case 1:
+
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        elapsed_time = elapsed_time_ms(&start_time, &end_time);
+        printf("time is %ld\n", elapsed_time);
+        int space;
+        space = (elapsed_time / 300);
+        element_pos += space;
+
+
+        //ascii += 1 << (7 - element_pos);
+        //printf("ascii is %d\n",ascii);
+            
+        
+        element_pos = (element_pos + 1) % 8;
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+        break;
+
+    case 2:
+        switch (signal)
+        {
+        case SIGUSR2:
+
+            ascii += 1 << (7 - element_pos);
+            
+        case SIGUSR1:
+        default:
+            element_pos = (element_pos + 1) % 8;
+            break;
+        }
+        if (element_pos == 0)
+        {
+
+            res[start] = ascii;
+            start++;
+            if (ascii == 10)
+            {
+                printf("!");
+                for (int i = 0;; i++)
+                {
+                    printf("%c", res[i]);
+                    if (res[i] == '\n')
+                    {
+                        break;
+                    }
+                }
+                //printf("!%s\n", res);
+                start = 0;
+                for (int i = 0; i < strlen(res) + 1; i++)
+                {
+                    res[i] = 0;
+                }
             }
             ascii = 0;
-            element_pos = 0;
+            
         }
-        ascii = 0;
-        element_pos = 0;
         
     }
 }
@@ -62,35 +129,44 @@ int main(int argc, char const *argv[])
     sa.sa_handler = handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
-    sa.sa_flags = SA_NODEFER;
+    sa.sa_flags = SA_RESTART;
     //copy form lab
 
+#if defined(SINGLE)
+    int mode = 1;
+    sigaction(SIGUSR1, &sa, NULL);
+#else
+    int mode = 2;
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
-        #if defined(SINGLE)
-        int single_mode = 1;
-    #else
-        int single_mode = 0;
-    #endif
-
-    int sent = 0;
+#endif
 
     pid_t pid;
     scanf("%d", &pid);
     //get the pid want to commnicate with
+    switch (mode)
+    {
+    case 1:
+        kill(pid, SIGUSR1);
+        break;
 
+    case 2:
+        kill(pid, SIGUSR2);
+        break;
+    }
+    usleep(200);
     while (1)
     {
 
         char sent_message[MAX_TEXT];
 
-        fgets(sent_message,MAX_TEXT,stdin);
+        fgets(sent_message, MAX_TEXT, stdin);
 
         //check if user enter a word
         if (sent_message[0] >= 33)
         {
+            printf("\n");
             //strcat(sent_message, "\n");
-
             for (int i = 0; i < strlen(sent_message); i++)
 
             //for each letter in the message
@@ -99,29 +175,45 @@ int main(int argc, char const *argv[])
                 for (int j = 7; j >= 0; --j)
                 //encode letter into binary, order of bit is from left to right
                 {
-                    if (sent_message[i] & (1 << j))
+                    switch (mode)
                     {
-                        //printf("1");
-                        kill(pid, SIGUSR1);
-                    }
-                    else
-                    {
-                        //printf("0");
-                        kill(pid, SIGUSR2);
-                    }
-                    
-                    usleep(300);
+                    case 1:
+                        if (sent_message[i] & (1 << j))
+                        {
+                            printf("1");
+                            usleep(450);
+                        }
+                        else
+                        {
+                            printf("0");
+                            
+                            kill(pid, SIGUSR1);
+                        }
+                        break;
 
-                    //putchar((sent_message[i]&(1<<j))?'1':'0');
+                    case 2:
+                        if (sent_message[i] & (1 << j))
+                        {
+
+                            kill(pid, SIGUSR2);
+                        }
+                        else
+                        {
+
+                            kill(pid, SIGUSR1);
+                        }
+                        break;
+                    }
+
+                    usleep(150);
                 }
-                
             }
-            
-            
         }
-        for(int i = 0; i <strlen(sent_message);i++){
-                sent_message[i] = 0;
-            }
+
+        for (int i = 0; i < strlen(sent_message); i++)
+        {
+            sent_message[i] = 0;
+        }
     }
 
     return 0;
